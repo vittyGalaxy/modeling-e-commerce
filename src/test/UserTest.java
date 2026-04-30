f8af153febd6f9fc9716cad0b60c85f0ef8bace2
Vittorione <vittorio.tiozzo.dev@gmail.com>
2026-04-30 23:25:11 +0200

    fixed UserTest

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -6,20 +6,28 @@ import static org.assertj.core.api.Assertions.*;
 public class UserTest {
     @Test
     void userNameSurnameRoleCorrectly() {
-        User u = new User("Vittorio", "Tiozzo", RoleType.client);
+        User u = new User("Vittorio", "Tiozzo", "01-05-2004",new Role(RoleType.client));
         assertThat(u.getName()).isEqualTo("Vittorio");
         assertThat(u.getSurname()).isEqualTo("Tiozzo");
-        assertThat(u.getRoleType()).isEqualTo(RoleType.client);
+        assertThat(u.getDate()).isEqualTo("01-05-2004");
+        assertThat(u.getRole()).isEqualTo(RoleType.client);
     }
 
     @Test
     void setterNameSurnameRoleTest() {
-        User u = new User("Vittorio", "Tiozzo", RoleType.client);
-        u.setRoleType(RoleType.catalogManager);
-        assertThat(u.getRoleType()).isEqualTo(RoleType.catalogManager);
+        User u = new User("Vittorio", "Tiozzo", new Role(RoleType.client));
+        u.setRole(new Role(RoleType.catalogManager));
+        assertThat(u.getRole()).isEqualTo(RoleType.catalogManager);
         u.setName("Name");
         assertThat(u.getName()).isEqualTo("Name");
         u.setSurname("Surname");
         assertThat(u.getSurname()).isEqualTo("Surname");
+        u.setSurname("02-08-2005");
+        assertThat(u.getSurname()).isEqualTo("02-08-2005");
+        List<String> lp = new List<String>();
+        lp.add("3333333333");
+        lp.add("2222222222");
+        u.setSurname(lp);
+        assertThat(u.getSurname()).isEqualTo(u.getListPhone());
     }
 }
